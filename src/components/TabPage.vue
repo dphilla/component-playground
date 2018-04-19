5fc4575ef00e8ae06eaedb92b3a0eca32a225741
Daniel Phillips <dan.phillips@aljex.com>
2018-04-19 11:46:55 -0600

    Add some tab stuff

---
+++ src/components/TabPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="tabs">
+    <b-card no-body>
+      <b-tabs :card='false' :lazy='true'>
+        <!-- Render Tabs -->
+        <b-tab :title="`Tab ${i}`" v-for="i in tabs" :key="i"> <!--- HOW RENDER DIFFERENT COMPONENTS IN TABS -->
+          Tab Contents {{i}}                                          <!--- HOW TO GET CLOSE-TAB IN TAB -->
+          <b-btn size="sm" variant="danger" class="float-right" @click="()=>closeTab(i)">
+            Close tab
+          </b-btn>
+        </b-tab>
+
+        <!-- New Tab Button (Using tabs slot) -->
+        <b-nav-item slot="tabs" @click.prevent="newTab" href="#">
+          +
+        </b-nav-item>
+
+        <!-- Render this if no tabs -->
+        <div slot="empty" class="text-center text-muted">
+          There are no open tabs
+          <br> Open a new tab using + button.
+        </div>
+      </b-tabs>
+    </b-card>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'TabPage',
+  data () {
+    return {
+      tabs: [],
+      tabCounter: 0
+    }
+  },
+  methods : {
+    closeTab (x) {
+      for (let i = 0; i < this.tabs.length; i++) {
+        if (this.tabs[i] === x) {
+          this.tabs.splice(i, 1)
+        }
+      }
+    },
+    newTab () {
+      this.tabs.push(this.tabCounter++)
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld  from '@/components/HelloWorld'
+import TabPage from '@/components/TabPage'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/tabs',
+      name: 'TabPage',
+      component: TabPage
     }
   ]
 })
