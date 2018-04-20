46f31be5aaf7722f653592d1e9374373b73f8d21
Daniel Phillips <dan.phillips@aljex.com>
2018-04-20 15:33:48 -0600

    Add handrolled layout

--- src/components/HandRolledTabs.vue
+++ src/components/HandRolledTabs.vue
@@ -1,87 +1,110 @@
 
 <template id="VueTabs">
-
-<div id="tabs">
-  <tabset>
-    <tab title="Home">
-      <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
-    </tab>
-    <tab title="Profile">
-      <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
-    </tab>
-    <tab title="Login">
-      <p>Login</p>
-    </tab>
-  </tabset>
-</div>
-
-
-
+  <div class="ui small form segment">
+    <div id="tabs">
+      <div class="ui  menu tabsName">
+         <div class="ui label blue item">
+           <i class="file outline icon"></i> Paginas:
+        </div>
+        <a class="item tabx" id="tAdd">
+          <i class="add square icon"></i></a>
+      </div>
+    </div>
+  </div>
+  <div class="ui dimmer modals page">
+    <div class="ui small modal">
+      <div class="header">
+        Cerrando..
+      </div>
+      <div class="content">
+        <p>¿Esta seguro de cerrar esta página?</p>
+      </div>
+      <div class="actions">
+        <div class="ui positive right labeled icon button">
+          Si
+          <i class="checkmark icon"></i>
+        </div>
+        <div class="ui negative button right labeled icon button">
+          No
+          <i class="close icon"></i>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 
-// WIP TODO CHANGE THIS INTO SINGLE-FILE COMPONENTS
+var numberTabs = 1;
+$(document).on('ready', function() {
+  $('#tabs .menu .tab').tab({});
+  appendTab('tabs', 'Tab 1');
+  appendTab('tabs', 'Demo 2', 'Custom content <u>Cool</u>');
+  appendTab('tabs', 'Demo 3', null, 'https://codepen.io');
+});
 
+function closeLoad(e) {
+  $("#" + 'loader-' + e).removeClass('active');
+}
 
-Vue.component('tabset', {
-  template: '#tabset-template',
-  data: function(){
-    return {
-      tabs: [],
-      current: null
-    }
-  },
-  methods: {
-    change: function(tab) {
-      this.current = tab;
-      this.tabs.forEach(function(_t) {
-        _t.active = _t === tab;
-      });
-    },
-    registerTab: function(tab) {
-      if (!this.current) {
-        this.current = tab;
-      }
-      this.tabs.push(tab);
+$(document).on('click', '#tAdd', function() {
+  appendTab('tabs');
+});
+
+$(document).on('click', '.btnx', function(e) {
+  deleteTab('tabs', e.target.id.replace('btnx-', ''));
+});
+
+function appendTab(tab, nombre, contenido, url) {
+  var t = $("#" + tab + " .tabsName");
+  var tt = $("#" + tab);
+  //var tl = t.find('.item').length;
+  var n = (!!nombre ? nombre : ("tab " + (numberTabs).toString()));
+  var tn = (!!nombre ? nombre.replace(/\s/g, '') : ("tab" + (numberTabs).toString()));
+  contenido = !!contenido ? contenido : 'Content ' + tn;
+  numberTabs++;
+  if (!$("#tab-" + tn).length) {
+    contenido = (!!url ? ('<iframe src="' + url + '" style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;border:none;" onload="closeLoad(\'' + tn + '\')"></iframe><div class="ui active dimmer" id="loader-' + tn + '"><div class="ui large text loader">Cargando ...</div></div>') : contenido);
+
+    t.find('#tAdd').remove();
+    t.append('<a class="item tab" data-tab="' + tn + '" id="tab-' + tn + '">' + n + ' <i class="close icon btnx" id="btnx-' + tn + '"></i></a>')
+      .append('<a class="item tabx" id="tAdd"><i class="add square icon"></i></a>');
+
+    tt.append('<div class="ui tab tabc segment" data-tab="' + tn + '" id="tab-c-' + tn + '">' + contenido + ' </div>')
+    $('#tabs .menu .tab').tab({});
+  }
+  $("#tab-" + tn).click();
+}
+
+function deleteTab(tab, nombre) {
+    var n = nombre;
+    var tt = $("#" + tab + " .tabsName");
+    var tl = tt.find('.item').length;
+    var t = $("#tab-" + n);
+    var tc = $("#tab-c-" + n);
+    if (!!n) {
+      $('.small.modal').modal({
+        closable: false,
+        onDeny: function() {
+          //alert('No!');
+          //return false;
+        },
+        onApprove: function() {
+          t.remove();
+          tc.remove();
+          if (tl > 0) {
+            var tb = $("#" + tab + " div a.tab")[tl - 4];
+            console.log(tb)
+            $(tb.click());
+            var u ="#"+tb.id;
+            console.log(u);
+            location.href=u;
+          }
+          $('#tabs .menu .tab').tab({});
+        }
+      }).modal('show');
     }
   }
-});
-Vue.component('tab', {
-  template: '#tab-template',
-  data: function() {
-    return {in: false};
-  },
-  computed: {
-    active: function() {
-      var isActive = this.$parent.current === this;
-      var self = this;
-
-      // or use this method:
-      // setTimeout(function() {
-      //   self.in = isActive;
-      // }, 0);
-      this.$nextTick(function() {
-        // if is commented, no animation
-        this.$els.panel.offsetWidth;  // force css reflow
-        this.in = isActive;
-      });
-      return isActive;
-    }
-  },
-  props: {
-    title: {
-      type: String,
-      required: true
-    }
-  },
-  compiled: function() {
-    this.$parent.registerTab(this);
-  }
-});
-// new Vue({
-  // el: '#tabs'
-// })
 
 </script>
 
